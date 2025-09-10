cad8cdebcdee5b30305ba5948d66f310f665df22
François Delbrayelle <fdelbrayelle@gmail.com>
2025-09-10 10:01:54 +0200

    fix(): add missing category to AI

---
+++ src/main/java/io/kestra/plugin/huggingface/package-info.java
@@ -0,0 +1,4 @@
+@PluginSubGroup(title = "Hugging Face", categories = PluginSubGroup.PluginCategory.AI)
+package io.kestra.plugin.huggingface;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
